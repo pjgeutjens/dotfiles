494b5ce06e72c47d656f7d4e48445bb751d834f9
Pieter Jan Geutjens <pieter.geutjens@leapconsulting.be>
2024-06-24 09:34:24 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,25 +9,17 @@ yay -Sy fzf stow
 git config --global user.email "pieter.geutjens@leapconsulting.be"
 git config --global user.name "Pieter Jan Geutjens"
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bashrc
-source .bashrc
-nvm install --lts
-
 mkdir tmp && cd tmp
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-yay -Sy arandr thunar --needed
 yay -Sy alacritty tmux neovim visual-studio-code-bin google-chrome-stable --needed
+yay -Sy acpi arandr archlinux-xdg-menu awesome-terminal-fonts dex dmenu dunst feh galculator gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb i3-wm i3blocks i3lock i3status jq lightdm lightdm-slick-greeter nwg-look mpv network-manager-applet numlockx playerctl polkit-gnome rofi scrot sysstat thunar thunar-archive-plugin thunar-volman tumbler unzip xarchiver xbindkeys xdg-user-dirs-gtk xed xfce4-terminal xorg-xbacklight xorg-xdpyinfo zip --needed
 yay -Sy awesome-terminal-fonts ttf-jetbrains-mono-nerd
 yay -Sy --needed i3blocks i3lock i3status rofi
-yay -Sy --needed network-manager-applet
-yay -Sy --needed thunar-archive-plugin unzip xarchiver zip feh
-
-curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | gpg --import -
-yay -Sy spotify
+yay -Sy network-manager-applet --needed
 
 cd ~
 git clone https://github.com/pjgeutjens/dotfiles.git && cd dotfiles
@@ -43,3 +35,10 @@ rm -rf ~/.config/tmux
 rm -rf ~/.config/nvim
 cd dotfiles/
 stow .
+
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | gpg --import -
+yay -Sy spotify
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bashrc
+source .bashrc
+nvm install --lts
